f050892f93dc4d3dc0900394f1f1f0404c3dcd6f
jangjaemin88 <142655432+jangjaemin88@users.noreply.github.com>
2023-09-02 04:12:49 +0000

    3주차 과제

---
+++ 3주차 과제.py
@@ -0,0 +1,12 @@
+1번 문제
+def calculate(numbers):
+    total = sum(numbers)
+    return total
+
+input_numbers = [1, 2, 3]
+result = calculate(input_numbers)
+print(result)
+
+2번 문제
+def함수부터 너무 어렵습니다...
+셀프 넘버가 무엇인지는 알겠는데 어떤식으로 구현해야 될지 모르겠어요
